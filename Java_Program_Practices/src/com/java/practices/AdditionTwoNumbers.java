4f15fb2de4e1c625dac0bca2f1a4322c72e6f27f
USER <USER@Anvika.alphionsee.in>
2026-02-07 19:06:17 +0530

    second programs

---
+++ Java_Program_Practices/src/com/java/practices/AdditionTwoNumbers.java
@@ -0,0 +1,12 @@
+package com.java.practices;
+
+public class AdditionTwoNumbers {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int a= 10, b = 20;
+		System.out.println(a + b);
+	}
+
+}
