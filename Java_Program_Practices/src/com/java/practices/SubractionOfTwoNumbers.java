4d39be594a66e78893ed7414ceffb2eb2493685c
USER <USER@Anvika.alphionsee.in>
2026-02-07 19:11:02 +0530

    thrid commit

---
+++ Java_Program_Practices/src/com/java/practices/SubractionOfTwoNumbers.java
@@ -0,0 +1,12 @@
+package com.java.practices;
+
+public class SubractionOfTwoNumbers {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int a = 30, b = 40;
+		System.out.println(a - b);
+	}
+
+}
